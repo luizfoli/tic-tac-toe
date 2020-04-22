9ab6640f422f9b9e46439bf6e14401565d9fb678
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-04-21 21:05:37 -0300

    feat: Adjusting functions to const

--- src/scripts.js
+++ src/scripts.js
@@ -1,29 +1,43 @@
 const table = document.querySelector('table');
 const tds = table.querySelectorAll('td');
 
-const players = ["X", "O"];
-let currentPlayer = 0;
-
+const xSquares = [];
+const oSquares = [];
 const squareAlreadyCheck = new Set();
 
-function checkSquare(square) {
-    squareAlreadyCheck.has(square) 
-        ? console.log('Square already add, select other')
-        : check(square);
-}
-
-function check(square) {
-    squareAlreadyCheck.add(square);
-    square.style.filter = "brightness(.6)";
-
-    const span = document.createElement('span');
-    span.textContent = players[currentPlayer];
-    square.append(span);
-
-    currentPlayer = +(!currentPlayer);
-} 
-
+const players = ["X", "O"];
+let currentPlayer = 0;
 
 tds.forEach((td) => {
     td.addEventListener('click', ({target}) => checkSquare(target));
 });
+
+const checkSquare = (square) => {
+    if(squareAlreadyCheck.has(square)) {
+        console.log('Square already add, select other')
+        return;
+    } 
+    
+    check(square);
+    changePlayer();
+}
+
+const check = (square) => {
+    squareAlreadyCheck.add(square);
+    changeSquareColor(square);  
+    addTextToSquare(square);
+} 
+
+const changeSquareColor = (square) => {
+    square.style.filter = "brightness(.6)";
+};
+
+const addTextToSquare = (square) => {
+    const span = document.createElement('span');
+    span.textContent = players[currentPlayer];
+    square.append(span);
+}
+
+const changePlayer = () => {
+    currentPlayer = +(!currentPlayer);
+}
