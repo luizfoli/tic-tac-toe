9a0506ae31b766f82c553c79a508a582c6d98ad2
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-04-21 21:56:55 -0300

    feat: Creating the logic to know who wins

--- src/scripts.js
+++ src/scripts.js
@@ -8,6 +8,17 @@ const squareAlreadyCheck = new Set();
 const players = ["X", "O"];
 let currentPlayer = 0;
 
+const winPossibilities = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+    ['1', '5', '9'],
+    ['3', '5', '7'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['3', '6', '9'],
+  ]
+
 tds.forEach((td) => {
     td.addEventListener('click', ({target}) => checkSquare(target));
 });
@@ -19,25 +30,57 @@ const checkSquare = (square) => {
     } 
     
     check(square);
+    const win = wins((currentPlayer ? oSquares : xSquares));
+
+    if(win) {
+        console.log(`The player ${players[currentPlayer]} wins!`)
+        return;
+    } 
+
     changePlayer();
 }
 
 const check = (square) => {
     squareAlreadyCheck.add(square);
     changeSquareColor(square);  
-    addTextToSquare(square);
+    addSquareToPlayer(square);
 } 
 
 const changeSquareColor = (square) => {
     square.style.filter = "brightness(.6)";
 };
 
-const addTextToSquare = (square) => {
-    const span = document.createElement('span');
-    span.textContent = players[currentPlayer];
-    square.append(span);
+const addSquareToPlayer = (square) => {
+    
+    const addTextToSquare = (square) => {
+        const span = document.createElement('span');
+        span.textContent = players[currentPlayer];
+        square.append(span);
+    }
+
+    const specifyPlayer = (square) => {
+        const squareIndex = square.id
+        currentPlayer ? oSquares.push(squareIndex) : xSquares.push(squareIndex);
+    }
+
+    addTextToSquare(square);
+    specifyPlayer(square);
 }
 
 const changePlayer = () => {
     currentPlayer = +(!currentPlayer);
+}
+
+function wins(squaresSelected) {
+    
+    for(let i = 0; i < winPossibilities.length; i++) {
+        let found = false;
+
+        const possibility = winPossibilities[i];
+        found = possibility.every(item => squaresSelected.includes(item))
+
+        if(found) {
+            return found;
+        }
+    }
 }
